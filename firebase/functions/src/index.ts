bac4ca9319e765a57926b85a53ecf8fa817c540f
Tanya Gray <tanya.gray@plexure.com>
2018-09-18 19:10:55 +1200

    fix(config): correctly export functions

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,4 +1,5 @@
-import * as functions from 'firebase-functions';
+import { helloWorld } from './hello-world';
+import { acquireTrademeToken } from './acquire-trademe-token';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,5 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export { helloWorld, acquireTrademeToken }
